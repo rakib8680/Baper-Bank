0f012cc66df2dab07e54f945338cf9425a180be0
rakib8680 <rakib.cst.1st@gmail.com>
2023-02-12 17:59:49 +0600

    taking input value by js

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// step 1
+// add eventHandler in submit button 
+
+document.getElementById('submit-btn').addEventListener('click', function () {
+
+
+    // step-2 : get email and password from input field
+    const email = document.getElementById('email');
+    const emailValue = email.value;            /*  always use   .value    to get values from input field*/
+
+
+    const passwordField = document.getElementById('password');
+    const password = passwordField.value;            /*  always use   .value    to get values from input field*/
+
+    // Danger!!!!! Never verify email and pass in future with the below process . Only for learning purpose.
+    // step-2 verify email and password 
+    if(emailValue == 'rakib@gmail.com' && password == 'Rakib1001'){
+        console.log('Welcome back Rakib');
+    }
+    else{
+        console.log('invalid username or password')
+    }
+})
